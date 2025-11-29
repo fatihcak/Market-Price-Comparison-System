e01eb9840c59ab8889a242487aa24e6fc193d86b
Doğukan Bilal Örs <165324202+DogukanOrs@users.noreply.github.com>
2025-11-30 00:36:47 +0300

    New components and UI design implemented (ongoing)

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,16 +1,10 @@
-// src/main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom'; // BU SATIR ÖNEMLİ
-import router from './router'; // BU SATIR ÖNEMLİ (router/index.tsx dosyamız)
-import './index.css'; 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    {/* BURASI ÇOK ÖNEMLİ!
-      Eski <App /> kodunu sildik, yerine bunu yazdık:
-    */}
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
