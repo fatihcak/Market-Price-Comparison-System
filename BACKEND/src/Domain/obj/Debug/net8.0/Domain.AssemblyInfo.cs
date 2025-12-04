8b40b0a0b458cfab372089935c488ae668e9857e
Fatih Çakır <fatcakirr@gmail.com>
2025-12-04 21:21:37 +0200

    AI-Son

--- BACKEND/src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ BACKEND/src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459a17a5ddd766c1fdb79414f4008ded8256e0ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea5bc5d27d776291416ba197dbc7efb634b0cef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
